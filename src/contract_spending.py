1373f1f68a2bf83b1fd1d4029ed297763d4d3375
Niels Mündler <n.muendler@posteo.de>
2023-03-08 12:01:11 +0100

    Fix comments

--- src/contract_spending.py
+++ src/contract_spending.py
@@ -5,7 +5,7 @@ def plus_one(i: int) -> int:
     return i * 2 + 1
 
 # This contract allows you to lock UTxOs at the script address with a given integer n as datum
-# The UTxO can only be spent if someone provides the nth fibonacci number as a redeemer
+# The UTxO can only be spent if someone provides the (n*2+1)th fibonacci number as a redeemer
 # This can be seen as a very simple kind of "proof of work" contract
 def validator(datum: int, redeemer: int, context: ScriptContext) -> None:
     assert fib(plus_one(datum)) == redeemer, "Wrong redeemer given"
